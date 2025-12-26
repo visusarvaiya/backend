333518ba804b81d4d514eaa14ea49cd074a4aa3a
visu sarvaiya <visusarvaiya54@gamil.com>
2025-12-26 12:08:08 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/ ', (req, res) => {
+  res.send('Hello World!')
+})
+app.get("/info",(req,res)=>{
+    res.send('my nam is visu')
+})
+
+app.listen(process.env.ports, () => {
+  console.log(`Example app listening on port ${port}`)
+})
